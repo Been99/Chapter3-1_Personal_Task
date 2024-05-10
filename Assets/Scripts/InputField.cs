663668465ca0d583a2437b9c04f05d1e755ae3b3
Been99 <167202496+Been99@users.noreply.github.com>
2024-05-10 20:41:12 +0900

    Add:StartScene & InputField.cs
    Scene추가 및 이름입력 받을 cs파일 추가 생성

---
+++ Assets/Scripts/InputField.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InputField : MonoBehaviour
+{
+    public Text text;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(GetComponent<InputField>() != null)
+        {
+            
+        }
+    }
+
+
+
+
+
+}
