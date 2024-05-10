230d43f18a8298d98aa04973daa51e0f49b48e1f
Been99 <167202496+Been99@users.noreply.github.com>
2024-05-10 16:04:39 +0900

    Add:Character Animation
    캐릭터의 애니메이션 생성

---
+++ Assets/Scripts/TopDownAimRotation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private Transform playerPivot;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    public void OnAim(Vector2 newAimDirecion)
+    {
+        RotateArm(newAimDirecion);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+
+        playerPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
